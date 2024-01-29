a3ced71efac35fdedefa292843a35cda4ed480b9
Gustavo <guatarashi@gmail.com>
2024-01-29 00:27:05 -0300

    feat: Aula 1 - Novos produtores e consumidores.

--- service-fraud-detector/src/main/java/br/com/atarashi/ecommerce/FraudeDetectorService.java
+++ service-fraud-detector/src/main/java/br/com/atarashi/ecommerce/FraudeDetectorService.java
@@ -2,7 +2,9 @@ package br.com.atarashi.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.math.BigDecimal;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class FraudeDetectorService {
 
@@ -17,7 +19,9 @@ public class FraudeDetectorService {
         }
     }
 
-    private void parse(ConsumerRecord<String, Order> record) {
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<Order>();
+
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
         System.out.println("----------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
@@ -30,6 +34,19 @@ public class FraudeDetectorService {
             // ignoring
             e.printStackTrace();
         }
-        System.out.println("Order processed");
+
+        var order = record.value();
+        if (isFraud(order)) {
+            //pretending that the fraud happens when the amount is >= 4500
+            System.out.println("Order is a fraud!!!!!");
+            orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getUserId(), order);
+        } else {
+            System.out.println("Approved: " + order);
+            orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getUserId(), order);
+        }
+    }
+
+    private static boolean isFraud(Order order) {
+        return order.getAmount().compareTo(new BigDecimal("4500")) >= 0;
     }
 }
